726e9f562ec5ad436932c175283dc835258f4a19
sapna <sapnakotian092@gmail.com>
2026-06-29 12:09:08 +0530

    updated project files

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
-
+import sys
+import os
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -13,12 +14,15 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+from database import Base
+from models.company import Company
+from models.job import Job
 
+target_metadata = Base.metadata
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
