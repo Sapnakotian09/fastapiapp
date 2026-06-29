611d93b0995aee9a00378b2a05d10ce1a3a36402
sapna <sapnakotian092@gmail.com>
2026-06-29 16:52:19 +0530

    updated project files

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,14 +1,18 @@
-import NavBar from './components/NavBar';
-import Welcome from './components/Welcome';
-import Footer from './components/Footer';
-
+import Welcome from "./components/Welcome";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
 function App(){
-  return(
+  return (
     <>
       <NavBar />
       <Welcome />
+      <br/>
+      <CompanyCard />
+      <JobCard />
       <Footer />
     </>
   )
 }
-export default App;
+export default App
